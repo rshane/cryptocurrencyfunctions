67e412498b73df59c2047d06782ac73926f80663
shane <rshane00@gmail.com>
2018-01-08 19:44:31 -0800

    setup

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require("express");
 var app = express();
+var cors = require('cors');
 app.use(function(req, res, next){
   console.log(`${req.method} request for '${req.url}'`);
   next();
-})
-app.use(express.static("./public"));
+});
+app.use(expres.static("./public"));
+app.use(cors());
+app.get("/test-api", function(req, res){
 
+});
 app.listen(8080);
 
 console.log("Express app running on port 8080");
