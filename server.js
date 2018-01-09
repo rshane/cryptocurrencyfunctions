b1caad9b3f4daf76b30f93c53ed7b7ba81244a2a
shane <rshane00@gmail.com>
2018-01-09 10:54:34 -0800

    about to work on front end basics

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
+import apiRouter from './api';
 var express = require("express");
-var app = express();
 var cors = require('cors');
-app.use(function(req, res, next){
+const server = express();
+server.set('view engine', 'ejs');
+server.use(cors());
+server.use(function(req, res, next){
   console.log(`${req.method} request for '${req.url}'`);
   next();
 });
-app.use(expres.static("./public"));
-app.use(cors());
-app.get("/test-api", function(req, res){
+server.use('/api', apiRouter);
+
+server.get("/", (req, res) => {
+  res.render('index', {
+    content: 'Hello Express and EJS!'
+  });
+})
+
+server.get("/test-api", function(req, res){
 
 });
-app.listen(8080);
+server.use(express.static("./public"));
+server.listen(8080);
 
 console.log("Express app running on port 8080");
 
-module.exports = app;
+module.exports = server;
